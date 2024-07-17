3f39478aacf111f29a54f0445261f129f21dd961
tonyloche <gomezant002@gmail.com>
2024-07-17 13:53:55 -0700

    update

---
+++ mongodb/pyMongo.py
@@ -0,0 +1,39 @@
+#make sure to use pip install pymongo
+from pymongo import MongoClient
+import json
+
+def main():
+
+    #connecting to database
+    client = MongoClient()
+    db = client.get_database("myfirstdb")
+    collection_movies = db.movies
+
+    #quering movies collection
+    movies = collection_movies.find()
+    for movie in movies:
+        print(movie)
+
+    #load in json
+    with open("orders.json") as f:
+        orders_data = json.load(f)
+    print(orders_data)
+
+    #insert json data to database
+    # db.orders.insert_many(orders_data)
+    # all_orders = db.orders.find()
+    # for order in all_orders:
+    #     print(order)
+
+    with open("employees.json") as e:
+        employees_data = json.load(e)
+    print(employees_data)
+
+    #insert employees data to database
+    # db.employees.insert_many(employees_data)
+
+    
+
+
+if __name__ == '__main__':
+    main()
