9bf9253530ccd2e909507bb43b59202ec29e0020
tonyloche <gomezant002@gmail.com>
2024-07-25 12:51:48 -0700

    connection to mongo db successful

--- project1/dbconnect.py
+++ project1/dbconnect.py
@@ -1,8 +1,68 @@
 from pymongo import MongoClient
+import re
 import json
 
+class DBConnect:
+    def __init__(self):
+        self.client = MongoClient('mongodb://localhost:27017/')
+        self.db = self.client['project1']
+        self.users_collection = self.db['users']
+        self.counter_collection = self.db['counters']
+
+    def get_next_sequence(self, sequence_name):
+        self.counter_collection.update_one(
+            {'_id': sequence_name},
+            {'$inc': {'sequence_value': 1}},
+            upsert=True
+        )
+        return self.counter_collection.find_one({'_id': sequence_name})['sequence_value']
+
+    def validate_password(self, password):
+        pattern = re.compile(r'^(?=.*[A-Z])(?=.*\W).{8,}$')
+        return pattern.match(password) is not None
+
+    def create_user(self, first_name, last_name, username, password):
+        if not self.validate_password(password):
+            print("Password does not meet the requirements.")
+            return
+        
+        user_id = self.get_next_sequence('user_id')
+        user = {
+            'id': user_id,
+            'first_name': first_name,
+            'last_name': last_name,
+            'username': username,
+            'password': password
+        }
+        self.users_collection.insert_one(user)
+        print("User created successfully.")
+
+    def login_user(self, username, password):
+        user = self.users_collection.find_one({'username': username, 'password': password})
+        if user:
+            print("Login successful.")
+        else:
+            print("Invalid username or password.")
+
 def main():
-    client = MongoClient('localhost', 27017)
-    db = client['project1']
+    db_connect = DBConnect()
+    
+    while True:
+        choice = input("Choose an option: [1] Login [2] Create New User [3] Exit: ")
+        if choice == '1':
+            username = input("Enter username: ")
+            password = input("Enter password: ")
+            db_connect.login_user(username, password)
+        elif choice == '2':
+            first_name = input("Enter first name: ")
+            last_name = input("Enter last name: ")
+            username = input("Enter username: ")
+            password = input("Enter password: ")
+            db_connect.create_user(first_name, last_name, username, password)
+        elif choice == '3':
+            break
+        else:
+            print("Invalid choice. Please select again.")
 
-
+if __name__ == "__main__":
+    main()
